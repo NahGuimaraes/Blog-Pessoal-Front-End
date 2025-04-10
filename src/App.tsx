50a30fd28b335b335abc735343c4bac6327bbf56
Najla Guimaraes <najlasabino.guimaraes@gmail.com>
2025-04-10 14:17:09 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import './App.css'
-
-function App() {
-  return (
-    <>
-        <BrowserRouter>
-          <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-  )
-}
-
-export default App
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+ import './App.css';
+ import Footer from './components/footer/Footer';
+ import Navbar from './components/navbar/Navbar';
+ import Home from './pages/home/Home';
+ import Login from './pages/login/Login';
+ import Cadastro from './pages/home/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listatemas/ListaTemas';
+import FormTema from './components/temas/formtema/FormTema';
+import DeletarTema from './components/temas/deletartema/DeletarTema';
+ 
+ function App() {
+   return (
+     <AuthProvider>
+       <BrowserRouter>
+         <Navbar />
+         <div className="min-h-[80vh]">
+           <Routes>
+           <Route path="/" element={<Login />} />
+               <Route path="/home" element={<Home />} />
+               <Route path="/cadastro" element={<Cadastro />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/temas" element={<ListaTemas />} />
+               <Route path="/cadastrartema" element={<FormTema />} />
+               <Route path="/editartema/:id" element={<FormTema />} />
+               <Route path="/deletartema/:id" element={<DeletarTema />} />
+           </Routes>
+         </div>
+         <Footer />
+       </BrowserRouter>
+     </AuthProvider>
+   );
+ }
+ 
+ export default App;
